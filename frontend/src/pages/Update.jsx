57a16cacb8cfc1716e0e7d178996afafea0f9fa8
Chris Lee <leechris0915@gmail.com>
2026-04-06 17:51:33 -0600

    added react template

---
+++ frontend/src/pages/Update.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const update= () => {
+    return (
+        <div>
+            <h1>update</h1>
+        </div>
+    )
+}
+
+export default update
---
+++ frontend/src/pages/User.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './User.css';
+
+const ACCOUNT_TYPES = [
+  { label: 'Student',     value: 'student' },
+  { label: 'Faculty',     value: 'faculty' },
+  { label: 'Company Rep', value: 'company' },
+];
+
+const emailPlaceholder = {
+  student: 'Personal Email',
+  faculty: 'University Email',
+  company: 'Business Email',
+};
+
+const defaultRegister = {
+  fname: '',
+  lname: '',
+  username: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+function User() {
+}
+
+export default User;
