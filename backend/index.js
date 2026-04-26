a36c4ef7ee86f674e1309c9a86775eb51ee1126f
Chris Lee <leechris0915@gmail.com>
2026-04-26 01:26:03 -0600

    removed password

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.listen(8800, () => {
     const db = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "Asiancanadian1!",
+        password: "",
         database: "unify"
     })
 
